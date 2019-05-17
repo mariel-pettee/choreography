b4f30174a080d61af32d33329bfc3091037daed2
mariel-pettee <mariel.pettee@gmail.com>
2019-05-17 15:12:35 -0400

    More renaming, split up notebooks by model

--- start_jupyter_notebook.sh
+++
@@ -1,9 +0,0 @@
-echo -e "
-Open up a new Terminal window and enter:
-ssh -N -f -L localhost:8888:localhost:8889 cuda@cuda.library.yale.edu
-
-Then open a new browser window and go to the URL: localhost:8888
-Copy-paste the token from the original window to log in.
-"
-# jupyter notebook --no-browser --port=8889
-jupyter-lab --no-browser --port=8889
